c004d11c8cb6baab271d69982edd335d466a239a
Juanma <juanmafallo@gmail.com>
2022-08-06 17:37:33 -0300

    singleton mongo connection

--- src/utils/mongoConnection.js
+++ src/utils/mongoConnection.js
@@ -1,6 +1,22 @@
-import mongoose from "mongoose";
+import { connection, connect } from "mongoose";
 
-const connectMongo = async () =>
-  mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+const isDbConnected = false;
+
+const connectMongo = async () => {
+  if (isDbConnected) return;
+  const db = await connect("mongodb://localhost:27017/test");
+  isDbConnected = db.connections[0].readyState;
+};
+
+connection
+  .on("connected", () => {
+    console.log("MongoDB is connected");
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("disconnected", () => {
+    console.log("MongoDB is disconnected");
+  });
 
 export default connectMongo;
