d08903a5bf90875b62094eb1713c91f801014ae5
Gianluca-W <Barreyro.gian@gmail.com>
2022-08-12 19:23:01 -0300

    pequeños ajustes

--- components/cards/LoginCard.tsx
+++ components/cards/LoginCard.tsx
@@ -6,32 +6,36 @@ export default function LoginCard() {
         backgroundImage: `url(${bgImg.src})`,
         backgroundPosition:"center",
         backgroundRepeat:"no-repeat",
-        backgroundSize:"cover"
+        backgroundSize:"cover",
+        
     }
     return (
-      <div style={style}>
+    <div style={style}>
+    <div style={{backgroundColor:"rgba(20,20,20,0.5)"}}> 
         <Container alignItems="center" css={{height:'100vh'}}>
-            
+                       
             <Grid.Container justify="center">
                 <Grid >
             <Card css={{ background: 'none' }}>
-                <Card.Header css={{paddingTop:"15vh"}}>
+                <Card.Header css={{paddingTop:"20vh"}}>
                     <Card.Image
                     src={loginLogo.src}
                     objectFit="cover"
-                    width="100%"
+                    width="70%"
                     height={"auto"}
                     alt="AlMargen Logo"
                     />
                 </Card.Header>
-                <Spacer y={2}/>
+                <Spacer y={5}/>
                 <Card.Footer>
-                    <Button size='lg' css={{marginLeft:"auto", marginRight:"auto", background:'White', color:'Black',textAlign:"center"}}>G Ingresa con google</Button>
+                    <Button size='lg' css={{marginLeft:"auto", marginRight:"auto", background:'White', color:'Black',textAlign:"center", width:"100%", height:"44px", fontFamily:"roboto",fontSize:"14px", fontWeight:"600",borderRadius:"8px"}}>G Ingresá con google</Button>
                 </Card.Footer>
             </Card>
             </Grid>
             </Grid.Container>
+            
         </Container>
+        </div>
     </div> 
     );
 }
