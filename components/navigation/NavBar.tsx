c5359b78448912de7778be51e000ee3907bc36cf
isa <isaarg2312@gmail.com>
2022-08-12 17:47:18 -0300

    update navbar

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -1,4 +1,6 @@
 import { Container, Row, Col, Card } from '@nextui-org/react';
+import { Button } from "@nextui-org/react";
+
 export default function NavBar() {
     var style = {
         navStyle: {
@@ -7,10 +9,9 @@ export default function NavBar() {
         }
     }
     return (
-        <Container fluid css={{backgroundColor:style.navStyle.primary,height:"44px",position:"fixed", zIndex:"1"}}>
-            <Card css={{backgroundColor:style.navStyle.accent,alignItems:"center",width:"115px",float:"right",font:"roboto"}}> 
-                Cerrar Sesion
-            </Card>
+        <Container fluid css={{backgroundColor:style.navStyle.primary,height:"8vh",display:"flex",position:"fixed", zIndex:"1",alignItems:"center",justifyContent:"space-between",padding:"2px 8px 2px 8px"}}>
+            casi
+            <Button css={{alignSelf:"right",backgroundColor:style.navStyle.accent}} size={"sm"}>Cerrar Sesión</Button>
         </Container>
     );
 }
