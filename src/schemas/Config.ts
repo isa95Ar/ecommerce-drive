131165657dc73aa827a340a161b9d7c27ab4ecfd
Juan Manuel Fallo <juanmafallo@gmail.com>
2022-08-26 16:06:24 -0300

    Config interface

---
+++ src/schemas/Config.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, model } from "mongoose";
+
+export interface ConfigI {
+  isOpen: boolean;
+}
+
+interface BaseConfigDocument extends ConfigI,Document {}
+
+const configSchema = new Schema<BaseConfigDocument>({
+  isOpen: {type: 'boolean', default: false}
+}, { capped: { size: 50000, max: 1} })
+
+if (!mongoose.models.Config){
+  model<BaseConfigDocument>("Config", configSchema); 
+}
+
+export default mongoose.models.Product;
