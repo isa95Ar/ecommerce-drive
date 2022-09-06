adad7bbea7957e235168a25d9e8e2606edecf74d
isa <isaarg2312@gmail.com>
2022-09-06 16:33:31 -0300

    update .env .gitignore and more

--- constants/config.ts
+++ constants/config.ts
@@ -1,13 +1,13 @@
 export default {
   gapi: {
-    SPREADSHEET_ID: "1TWtQUwrYNQ0I4TY36UnLkhOCiqWVBeWgWiPO8wxJlSY",
-    SCOPES: "https://www.googleapis.com/auth/spreadsheets",
-    PRODUCT_SHEET_NAME: "products",
-    USERS_SHEET_NAME:"socios",
-    OAUTH_CLIENT_ID: "87845224128-7us8ca317i9pojma6ol7m3ujnp7slqfc.apps.googleusercontent.com",
-    OAUTH_CLIENT_KEY: "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",
-    OAUTH_REDIRECT_URL: "http://localhost:3000/api/oauthcallback",
-    OAUTH_SCOPES: ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+    SPREADSHEET_ID: process.env.SPREADSHEET_ID,
+    SCOPES: process.env.SCOPES,
+    PRODUCT_SHEET_NAME: process.env.PRODUCT_SHEET_NAME,
+    USERS_SHEET_NAME:process.env.USERS_SHEET_NAME,
+    OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
+    OAUTH_CLIENT_KEY: process.env.OAUTH_CLIENT_KEY,
+    OAUTH_REDIRECT_URL: process.env.OAUTH_REDIRECT_URL,
+    OAUTH_SCOPES: [process.env.OAUTH_SCOPES]
   },
-  IRON_SESSIONS_PASSWORD :"8BV9cARYpoTXzesGTu3i594wVwXdyzeb"
+  IRON_SESSIONS_PASSWORD :process.env.IRON_SESSIONS_PASSWORD
 };
