9de68ec257568a61500bbc048e289bc21fabf0f8
Matias Soto <matias@animus.com.ar>
2022-09-27 12:52:39 -0300

    fix mac sed

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -8,6 +8,9 @@ echo "Building Apps"
  npm i
  npm run build
  npm run build-command
+ if [[ $OSTYPE == 'darwin'* ]]; then
+  alias sed=gsed
+ fi
  sed -i"" "3i\var dotEnv = require('dotenv');" build/constants/config.js
  sed -i""  "4i\dotEnv.config();" build/constants/config.js
 
