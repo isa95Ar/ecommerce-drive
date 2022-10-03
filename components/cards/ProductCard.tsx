baa7f061477f4e0d874901fe520b3982915f126e
Matias Soto <matias@animus.com.ar>
2022-10-03 16:08:32 -0300

    Fix qty and total on products

--- components/cards/ProductCard.tsx
+++ components/cards/ProductCard.tsx
@@ -18,10 +18,10 @@ const ProductCard: FC<props> = ({ item, addProduct }) => {
 	const cart = useCart();
 	const [quantity, setQuantity] = useState(1);
 
-	useEffect(() => {
-		const qty = cart.getCartProductQty(item.code);
-		setQuantity(qty);
-	}, [cart.data]);
+	// useEffect(() => {
+	// 	const qty = cart.getCartProductQty(item.code);
+	// 	setQuantity(qty);
+	// }, [cart.data]);
 	return (
 		<Grid xs={12} sm={12} md={12} lg={12} xl={12}>
 			<Card css={{ margin: 0, letterSpacing: 0 }}>
