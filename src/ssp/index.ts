59713912a8b75d6e9c0665c119c1bb262bb140ce
Juan Manuel Fallo <juanmafallo@gmail.com>
2022-10-14 16:36:25 -0300

    Get server side props fix

--- src/ssp/index.ts
+++ src/ssp/index.ts
@@ -3,22 +3,39 @@ import ConfigService from '../services/ConfigService';
 import { getIronSession, IronSessionData } from 'iron-session';
 import { sessionOptions } from '../utils/withIronSession';
 import { UserLogged } from '../global/types';
+import OrderService from '../services/OrderService';
 
 export async function getServerSideProps(context) {
 	const configService = container.resolve(ConfigService);
 	const getIsOpen = await configService.getCartStatus();
 	const ironSession: IronSessionData = await getIronSession(context.req, context.res, sessionOptions);
 
+	const cart = { products: [], total: 0 };
+
 	if (ironSession.user && getIsOpen.status === 'open') {
+		const orderService = container.resolve(OrderService);
+		const ModelResponse = await orderService.getUserOrder(ironSession.user.email);
+		if (ModelResponse) {
+			cart.products = ModelResponse.products.map(({ code, name, price, minimum, qty, total, picture }) => ({
+				code,
+				name,
+				price,
+				minimum,
+				qty,
+				total,
+				picture
+			}));
+			cart.total = cart.products.reduce((total, product) => total + product.total, 0);
+		}
 		return {
 			redirect: {
 				permanent: false,
 				destination: '/products'
 			},
-			props: {}
+			props: { cart, user: { logged: false } }
 		};
 	}
 
 	const user: UserLogged = ironSession.user ?? { logged: false };
-	return { props: { cartStatus: getIsOpen, user } };
+	return { props: { cartStatus: getIsOpen, user, cart } };
 }
