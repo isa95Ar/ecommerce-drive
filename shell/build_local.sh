bc7ad7e5875ecbc9f919a475523e3cbfeb044a39
isa <isaarg2312@gmail.com>
2023-02-28 14:40:46 -0300

    builing command for local

---
+++ shell/build_local.sh
@@ -0,0 +1,8 @@
+#building command
+echo "building Command"
+ npm run build-command
+ if [[ $OSTYPE == 'darwin'* ]]; then
+  alias sed=gsed
+ fi
+ sed -i"" "3i\var dotEnv = require('dotenv');" build/constants/config.js
+ sed -i""  "4i\dotEnv.config();" build/constants/config.js
