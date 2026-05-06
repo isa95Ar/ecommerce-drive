4b19b6014320ed1270cefbe06110dcac507e765e
isaias <isaarg2312@gmail.com>
2026-05-06 15:44:43 -0300

    builded -v 0.1.0

--- build/constants/config.js
+++ build/constants/config.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
+var dotEnv = require('dotenv');
+dotEnv.config();
 exports["default"] = {
     gapi: {
         SPREADSHEET_ID: process.env.SPREADSHEET_ID,
